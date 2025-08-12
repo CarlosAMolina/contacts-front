a303ec8da27507f56540fd066ead4fbfb13d3ee3
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2025-08-12 22:03:41 +0200

    style(eslint): apply

--- src/server.js
+++ src/server.js
@@ -4,33 +4,33 @@ import http from 'node:http'
 import { contacts } from './fake.js'  // TODO not use fake values
 
 export const start = () => {
-  const port = typeof process.env.FRONT_PORT === 'undefined' ? 5000 : process.env.FRONT_PORT;
-  const server = createServer();
-  server.listen(port, () => {
-    const address = `http://localhost:${port}`
-    console.log(`server on ${address}`);
-  });
+    const port = typeof process.env.FRONT_PORT === 'undefined' ? 5000 : process.env.FRONT_PORT;
+    const server = createServer();
+    server.listen(port, () => {
+        const address = `http://localhost:${port}`
+        console.log(`server on ${address}`);
+    });
 }
 
 const createServer = () => {
-  return http.createServer(async (req, res) => {
-    const HTML_PATH = new URL('./template.html', import.meta.url).pathname;
-    const template = await fs.readFile(HTML_PATH, 'utf-8');
-    const html = interpolate(template, {contacts: formatContacts(contacts)});
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end(html);
-  });
+    return http.createServer(async (req, res) => {
+        const HTML_PATH = new URL('./template.html', import.meta.url).pathname;
+        const template = await fs.readFile(HTML_PATH, 'utf-8');
+        const html = interpolate(template, {contacts: formatContacts(contacts)});
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(html);
+    });
 }
 
 const interpolate = (html, data) => {
-  return html.replace(/\{\{\s*(\w+)\s*\}\}/g, (match, placeholder) => {
-    return data[placeholder] || '';
-  });
+    return html.replace(/\{\{\s*(\w+)\s*\}\}/g, (match, placeholder) => {
+        return data[placeholder] || '';
+    });
 }
 
 const formatContacts = (contacts) => {
-  return contacts.map(contact => {
-    return `
+    return contacts.map(contact => {
+        return `
       <div class="note">
         <p>${contact.name}</p>
         <div class="tags">
@@ -38,5 +38,5 @@ const formatContacts = (contacts) => {
         </div>
       </div>
     `
-  }).join('\n')
+    }).join('\n')
 }
